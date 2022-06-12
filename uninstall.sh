7f88cc0319ca853c72932a9f333435260d4a17ec
frazzlesgrant1994 <39445054+frazzlesgrant1994@users.noreply.github.com>
2022-06-12 22:53:45 +0100

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,2 +1,33 @@
+#!/bin/bash
+
+## Linux-Toolkit Uninstaller
+## Simple script to remove the files that are used with Linux-Toolkit
+## Author: Frazer Grant
+## Version: 1.0
+
+
+
+file=/etc/Linux-toolkit/linux-toolkit.sh
+filedesktop=/usr/share/applications/toolkit.desktop
+
+pause(){
+	read -n 1 -s -r -p "Press any key to continue"
+}
+
+
+if [ -f "$file" ]; then
 sudo rm -rf /etc/Linux-toolkit
+else 
+exit 1 
+
+fi 
+
+if [ -f "$filedesktop" ]; then
 sudo rm -rf /usr/share/applications/toolkit.desktop
+else 
+exit 1
+fi
+
+
+echo "Linux Toolkit has been removed"
+pause
