f20745945bac5cfe7735c5f015a744cb6c764e70
frazzlesgrant1994 <39445054+frazzlesgrant1994@users.noreply.github.com>
2022-06-13 09:00:38 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,37 +5,43 @@
 ## Version: 1.0
 
 
-
+# File locations
 shfile=/etc/Linux-toolkit/linux-toolkit.sh
 iconfile=/etc/Linux-toolkit/tool.jpeg
 filedesktop=/usr/share/applications/toolkit.desktop
+
+
 
 pause(){
 	read -n 1 -s -r -p "Press any key to continue"
 }
 
 
+# Checking if the .sh file existes
 if [ -f "$shfile" ]; then
-echo "Linux-toolkit.sh is already installed "
+	echo "Linux-toolkit.sh is already installed "
 else 
-sudo mkdir /etc/Linux-toolkit
-sudo cp linux-toolkit.sh /etc/Linux-toolkit/
-
+	sudo mkdir /etc/Linux-toolkit
+	sudo cp linux-toolkit.sh /etc/Linux-toolkit/
 fi 
 
+
+# Checking if the icon existes
 if [ -f "$iconfile" ]; then
-echo "Linux-toolkit Icons already installed"
+	echo "Linux-toolkit Icons already installed"
 else 
-sudo cp  tool.jpg /etc/Linux-toolkit/
-
+	sudo cp  tool.jpg /etc/Linux-toolkit/
 fi 
+
+
+# Checking if the .desktop file existes
 if [ -f "$filedesktop" ]; then
-echo "Linux-toolkit shortcut already installed"
+	echo "Linux-toolkit shortcut already installed"
 else 
-sudo cp  toolkit.desktop /usr/share/applications/
+	sudo cp  toolkit.desktop /usr/share/applications/
 fi
 
-sudo chmod +rwx /etc/Linux-toolkit/linux-toolkit.sh
+	sudo chmod +rwx /etc/Linux-toolkit/linux-toolkit.sh
 
-echo "Linux Toolkit has been Installed"
+	echo "Linux Toolkit has been Installed"
 pause
